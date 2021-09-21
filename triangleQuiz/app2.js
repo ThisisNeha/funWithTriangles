ca1b4b1558ab60e47ac0be1e640eda2fedddf60c
NehaBarak <nehabarak45@gmail.com>
2021-09-21 21:33:16 +0530

    docs: changes in documentation

--- triangleQuiz/app2.js
+++ triangleQuiz/app2.js
@@ -1,23 +1,21 @@
-const quizForm= document.querySelectorAll(".quiz-form");
 const button = document.querySelector("#btn");
 const outputScore = document.querySelector("#output");
 
-const correctAnswers = ['90°', 'right angled']; 
+const correctAnswers = ["90°", "right angled"];
 
-function calculateScore(){
-let score = 0;
-let index= 0;
-const result = new FormData("quizForm");
-console.log(result);
-for(let value of result.values()){
-    if(value === correctAnswers[index]){
-        score= score + 1;
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const result = new FormData(document.getElementById("quiz-form"));
+ 
+  for (let value of result.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
     }
-    index= index + 1;
+    index = index + 1;
+  }
+  outputScore.innerText = "Your score is " + score;
+  // console.log(calculateScore);
 }
-outputScore.innerText = "Your score is " + score;
-// console.log(calculateScore);
-}
-
 
 button.addEventListener("click", calculateScore);
