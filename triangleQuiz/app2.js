1fe23b1a1ab58d1c0b742d9db53a47bcfd99d430
NehaBarak <nehabarak45@gmail.com>
2021-09-21 21:20:03 +0530

    update:

--- triangleQuiz/app2.js
+++ triangleQuiz/app2.js
@@ -7,12 +7,13 @@ const correctAnswers = ['90°', 'right angled'];
 function calculateScore(){
 let score = 0;
 let index= 0;
-const quiz = new FormData(quizForm);
-for (let value of quiz.values()){
-    if(value===correctAnswers[index]){
-        score ++;
+const result = new FormData("quizForm");
+console.log(result);
+for(let value of result.values()){
+    if(value === correctAnswers[index]){
+        score= score + 1;
     }
-    index ++;
+    index= index + 1;
 }
 outputScore.innerText = "Your score is " + score;
 // console.log(calculateScore);
